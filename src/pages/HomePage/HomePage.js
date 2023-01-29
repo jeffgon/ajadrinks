954b42478495686d0aab51e0bcdf1954fa5beb5b
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-01-29 16:28:00 -0300

    TopContainer of Homeome

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,9 +5,21 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Header from "../../components/Heander.js"
+import { MainContainer, TopContainer } from "./styled.js"
+import capaHome from "../../assets/capaHome.png"
 
 export default function HomePage(){
     return(
-        <Header />
+        <>
+            <Header />
+            <MainContainer>
+                <TopContainer>
+                    <h2>Bem-vindo a melhor experiência</h2>
+                    <h2>mixológica das suas papilas!</h2>
+                    <img src={capaHome} alt="imagem capa diversas garrafas de bebidas"/>
+                </TopContainer>
+
+            </MainContainer>
+        </>
     )
 }
